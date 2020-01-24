00a1ce1d28ca0b2a682763deaf1acb1af20b7329
Kevin Wenger <wenger.kev@gmail.com>
2020-01-24 17:36:07 +0100

    reset drush.sh & phpunit-core script.sh file

--- functions/drush.sh
+++ functions/drush.sh
@@ -6,22 +6,14 @@
 # Ensures that the right drush version is installed.
 #
 function drupal_ti_ensure_drush() {
-  # Check if drush is already available.
-	DRUSH=$(which drush || echo "")
-
-	if [ -n "$DRUSH" ]
-	then
-		echo "Drush is already installed on: $DRUSH"
-		echo "Drush version:"
-		echo "${drush --version}"
-		return
-	fi
-
 	# This function is re-entrant.
-  if [ -r "$TRAVIS_BUILD_DIR/../drupal_ti-drush-installed" ]
+	if [ -r "$TRAVIS_BUILD_DIR/../drupal_ti-drush-installed" ]
 	then
 		return
 	fi
+
+	# Check if drush is already available.
+	DRUSH=$(which drush || echo "")
 
 	if [ -z "$DRUSH" ]
 	then
--- runners/phpunit-core/script.sh
+++ runners/phpunit-core/script.sh
@@ -2,20 +2,12 @@
 # @file
 # Simple script to run the core phpunit tests via travis-ci.
 
-echo "Run core tests"
 cd "$DRUPAL_TI_DRUPAL_DIR"
 
 # Find absolute path to modules directory.
 MODULE_DIR=$(cd "$DRUPAL_TI_MODULES_PATH"; pwd)
 
 ARGS=( $DRUPAL_TI_PHPUNIT_ARGS )
-
-echo "run PHPUnit with ARGS ${ARGS[@]} on '$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY'"
-
-echo "debug list"
-ls -la "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY"
-echo "debug list 2"
-ls -la "$MODULE_DIR"
 
 # Run core tests
 cd core
